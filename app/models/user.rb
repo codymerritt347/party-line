55f8cec95916f522345b9c038362276e95129e15
codymerritt347 <cody.merritt@icloud.com>
2021-04-09 12:04:12 -0400

    Add #full_name to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   has_many :messages
   has_many :plans, through: :groups
   has_many :replies
+
+  def full_name
+    if self.first_name && self.last_name
+      "#{self.first_name} #{self.last_name}"
+    else
+      self.first_name
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'about', to: 'users#hello'
   resources :plans
   resources :replies
   resources :messages
