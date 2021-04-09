cd5c8523e0c93f1f6460f00ee044b38e1c4b889a
codymerritt347 <cody.merritt@icloud.com>
2021-04-09 11:35:47 -0400

    Establish relationships betweens models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :user_groups
+  has_many :groups, through: :user_groups
+  has_many :messages
+  has_many :plans, through: :groups
+  has_many :replies
 end
