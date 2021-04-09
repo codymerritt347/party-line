10e7a46c123941e3d7a7be4d14a868b9e5cabcb3
codymerritt347 <cody.merritt@icloud.com>
2021-04-09 11:42:56 -0400

    Add references columns to models with belongs_to relationships

--- db/migrate/20210409153011_create_replies.rb
+++ db/migrate/20210409153011_create_replies.rb
@@ -2,6 +2,8 @@ class CreateReplies < ActiveRecord::Migration[6.1]
   def change
     create_table :replies do |t|
       t.text :content
+      t.references :user, null: false, foreign_key: true
+      t.references :message, null: false, foreign_key: true
 
       t.timestamps
     end
