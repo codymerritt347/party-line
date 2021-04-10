de50ef856ca20f3bba2e6e7470102b24b685a9b7
codymerritt347 <cody.merritt@icloud.com>
2021-04-10 11:51:57 -0400

    Add basic CNew Party, New Message, and Edit User buttons

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,8 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @post.valid?
-      @post.save
+    if @user.valid?
+      @user.save
       redirect_to user_path(@user)
     else
       render :new
