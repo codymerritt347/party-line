184edb64f1e7869a565d15edcaf5df560a3e1c24
codymerritt347 <cody.merritt@icloud.com>
2021-04-16 14:06:03 -0400

    Fill out basic Status Controller with strong params

---
+++ app/controllers/statuses_controller.rb
@@ -0,0 +1,51 @@
+class StatusesController < ApplicationController
+  def index
+    @statuses = Status.all
+  end
+
+  def new
+    @status = Status.new
+  end
+
+  def create
+    @status = Status.new(status_params)
+    if @status.valid?
+      @status.save
+      redirect_to status_path(@status)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @status = Status.find(params[:id])
+  end
+
+  def edit
+    @status = Status.find(params[:id])
+  end
+
+  def update
+    @status = Status.find(params[:id])
+    @status.update(status_params)
+    redirect_to status_path(@status)
+  end
+
+  def destroy
+    status = Status.find(params[:id])
+    if status
+      status.destroy
+      redirect_to statuses_path, notice: "Status deleted"
+    else
+      redirect_to statuses_path, notice: "Status not found"
+    end
+  end
+
+  private
+
+  def status_params
+    params.require(:status).permit(
+      :content
+    )
+  end
+end
