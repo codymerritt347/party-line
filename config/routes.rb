57fefd25b8ddc01f5706b81b837027b8a452a2ca
codymerritt347 <cody.merritt@icloud.com>
2021-04-16 11:00:58 -0400

    Add Static Controller and Static welcome view

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :replies
   resources :tasks
   resources :users
+
+  get '/', to: 'static#welcome'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
