143e036541d94db66fa56910154175abcc6d4775
codymerritt347 <cody.merritt@icloud.com>
2021-04-20 12:13:38 -0400

    Edit root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "static#welcome"
+  get '/login', to: "sessions#new"
+  post '/login', to: "sessions#create"
+  post '/logout', to: "sessions#destroy"
+
   resources :users do
     resources :statuses
     resources :friendships
@@ -14,9 +19,4 @@ Rails.application.routes.draw do
     resources :replies, only: [:index, :show]
   end
   resources :replies
-  
-  get '/', to: 'static#welcome'
-  get '/login', to: "sessions#new"
-  post '/login', to: "sessions#create"
-  post '/logout', to: "sessions#destroy"
 end
