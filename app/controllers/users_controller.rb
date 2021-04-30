da5a92ee37929e0400d4eeebacd360fd44db07a8
codymerritt347 <cody.merritt@icloud.com>
2021-04-30 10:27:54 -0400

    Add Sessions routes for new/create/destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to @user, success: "User successfully created!"
     else
       flash.now[:error] = @user.errors.full_messages.to_sentence
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "static#welcome"
-  get '/login', to: "sessions#new"
-  post '/login', to: "sessions#create"
-  post '/logout', to: "sessions#destroy"
+  
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'signup', to: "users#new", as: 'signup'
+  post 'login', to: "sessions#new", as: 'login'
+  post 'logout', to: "sessions#destroy", as: 'logout'
 
   resources :users do
     resources :statuses
-    resources :messages
   end
   resources :friendships
   resources :parties
