f266343d936ecb0f717d084244d2851bd14d631f
codymerritt347 <cody.merritt@icloud.com>
2021-04-30 10:19:43 -0400

    Add #create, #destroy actions to Sessions controller

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -3,9 +3,17 @@ class SessionController < ApplicationController
   end
 
   def create
-    session[:current_user_id] = @user.id
+    @user = User.find_by_email(params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to @user, notice: "Logged In!"
+    else
+      render :new
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: "Logged Out!"
   end
 end
