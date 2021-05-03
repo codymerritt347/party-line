96dc9d23a559ee2c23dc4cec00235dfbed1331a0
codymerritt347 <cody.merritt@icloud.com>
2021-05-03 15:15:07 -0400

    Add content length validation to Status

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to @user, notice: "Logged In!"
     else
-      render :new
+      render :new, notice: "Incorrect!"
     end
   end
 
--- app/models/status.rb
+++ app/models/status.rb
@@ -1,6 +1,13 @@
 class Status < ApplicationRecord
   belongs_to :user
 
-  validates :content, { presence: true }
+  validates :content, { 
+    presence: true,
+    length: {
+      minimum: 1,
+      maximum: 281,
+      message: "Must contain 1-281 characters!"
+    }
+  }
   
 end
